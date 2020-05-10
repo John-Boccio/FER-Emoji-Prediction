23410347f045086b7b1593611f3779e2aeb1c99e
John Boccio <john.t.boccio@gmail.com>
2020-05-10 00:13:59 -0400

    Fixed GUI scale

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from PIL import Image, ImageTk
 
 import cv2
 from FER import fer
+import matplotlib.pyplot as plt
 import tkinter as tk
 from tkinter import ttk
 
@@ -11,26 +12,44 @@ class App(object):
         self.cnn = cnn
         self.img_transform = img_transform
 
+        self.width = 1080
+        self.height = 720
+        self.padx = 10
+        self.pady = 10
+        self.img_width = int((self.width - 4*self.padx) / 2)
+        img_width_ratio = self.img_width / self.width
+        self.img_height = int(self.height * img_width_ratio)
+
         self.video_stream = cv2.VideoCapture(0)
+
+        self.expressions = [expr.name for expr in fer.FerExpression]
 
         self._init_gui()
 
     def _init_gui(self):
         self.window = tk.Tk()
         self.window.title("FER Emoji Prediction")
-        self.window.geometry('720x480')
+        self.window.geometry(f'{self.width}x{self.height}')
+        # Don't allow any resizing
+        self.window.resizable(0, 0)
 
-        blank_profile_picture = ImageTk.PhotoImage(Image.open('images/blank-profile-picture.jpg'))
-        self.face_label = ttk.Label(image=blank_profile_picture)
-        self.face_label.image = blank_profile_picture
-        self.face_label.pack(side=tk.LEFT, padx=10, pady=10)
+        self.face_label = ttk.Label()
+        self.face_label.grid(row=0, column=0, padx=self.padx, pady=self.pady)
+        self._update_face(cv2.imread("images/blank-profile-picture.jpg"))
 
-        self.capture_image = ttk.Button(
-            self.window,
-            text="Capture Image",
-            command=self._capture_image_clicked
-        )
-        self.capture_image.pack(side=tk.BOTTOM, padx=10, pady= 10)
+        self.graph_label = ttk.Label()
+        self.graph_label.grid(row=0, column=1, padx=10, pady=10)
+        self.fig, self.ax = plt.subplots()
+        plt.title("Probabilities For Each Expression")
+        plt.xlabel("Expression")
+        plt.xticks(rotation=45)
+        plt.ylabel("Probability")
+        plt.ylim(0.0, 1.0)
+        plt.tight_layout()
+        self._update_graph([1/len(self.expressions)] * len(self.expressions))
+
+        self.find_emojis_button = ttk.Button(self.window, text="Find Emoji's", command=self._find_emojis_event)
+        self.find_emojis_button.grid(row=1, columnspan=2, padx=self.padx, pady=self.pady)
 
     def run(self):
         self.window.mainloop()
@@ -39,16 +58,28 @@ class App(object):
         self.video_stream.release()
 
     def _update_face(self, image):
-        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        image_tk = ImageTk.PhotoImage(Image.fromarray(image_rgb))
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = Image.fromarray(image)
+        image = image.resize((self.img_width, self.img_height))
+        image_tk = ImageTk.PhotoImage(image)
         self.face_label.configure(image=image_tk)
         self.face_label.image = image_tk
 
-    def _capture_image_clicked(self):
+    def _update_graph(self, probabilities):
+        plt.bar(self.expressions, probabilities)
+        plt.savefig("images/plot.jpg", dpi=1200, quality=100)
+        image = Image.open("images/plot.jpg")
+        image = image.resize((self.img_width, self.img_height))
+        image_tk = ImageTk.PhotoImage(image)
+        self.graph_label.configure(image=image_tk)
+        self.graph_label.image = image_tk
+
+    def _find_emojis_event(self):
         _, image = self.video_stream.read()
         face = fer.find_face(image)
         if face is None:
             self._update_face(image)
+            self._update_graph([0.0] * len(self.expressions))
             print("No face found")
             return
 
@@ -58,5 +89,6 @@ class App(object):
         cv2.rectangle(image, (left, top), (right, bottom), (0, 255, 0), 2)
         cv2.putText(image, expression, (left, top - 5), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
         self._update_face(image)
+        self._update_graph(probabilities)
 
         print(f"Image Captured -\tExpression: {expression:<10}\tProbabilities: {probabilities}")
